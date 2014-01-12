d1e777a0e68a7fafa94908b2be6a680646b06145
Matt Morris <matt@mattmorr.is>
2014-01-12 15:29:45 -0500

    Some final cleanup.

--- main.py
+++ main.py
@@ -6,24 +6,6 @@ from rauth import OAuth1Session, oauth
 from FitbitAuth import *
 from TwitterAuth import *
 
-def get_floors_today():
-  base_url = "https://api.fitbit.com"
-  request_token_url = base_url + "/oauth/request_token"
-  access_token_url = base_url + "/oauth/access_token"
-  authorize_url = "http://www.fitbit.com/oauth/authorize"
-  api_version = "/1"
-
-  session = OAuth1Session(fitbit_consumer_key(),
-                          fitbit_consumer_secret(),
-			  fitbit_oauth_token(),
-			  fitbit_oauth_secret())
-
-  today = date.today().isoformat()
-  url = base_url + api_version + "/user/-/activities/date/"+today+".json"
-  r = session.get(url, params={}, header_auth=True)
-  return int(r.json()['summary']['floors'])
-
-
 def get_floors_ytd():
   base_url = "https://api.fitbit.com"
   request_token_url = base_url + "/oauth/request_token"
@@ -89,18 +71,9 @@ def send_direct_message(user, message_body):
 
 
 def main():
-  #num = get_floors_ytd()
-  #tweet_text = 'Floors climbed year-to-date: ' + str(num) #+ ' #fitbit #fitstats'
-  #postTweet(tweet_text)
-
-  print get_ytd_pace()
-  print get_floors_today()
-
-  pace_delta = get_floors_ytd() - get_ytd_pace()
-  if pace_delta > 0:
-    send_direct_message('boxel', "You're ahead of pace by " + str(pace_delta))
-  else:
-    send_direct_message('boxel', "You're behind pace by " + str(abs(pace_delta)))
+  num = get_floors_ytd()
+  tweet_text = 'Floors climbed year-to-date: ' + str(num) + ' #fitbit #fitstats'
+  postTweet(tweet_text)
 
 if __name__ == "__main__":
     main()
