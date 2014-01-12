be32b4bbbb8f1345be24c20dec16b3ff9b3bb0e1
Matt Morris <matt@mattmorr.is>
2014-01-12 09:41:49 -0500

    Added twitter DM capability. Also started some logic for messaging you based on a yearly pace.

--- main.py
+++ main.py
@@ -6,7 +6,25 @@ from rauth import OAuth1Session, oauth
 from FitbitAuth import *
 from TwitterAuth import *
 
-def getYTDFloors():
+def get_floors_today():
+  base_url = "https://api.fitbit.com"
+  request_token_url = base_url + "/oauth/request_token"
+  access_token_url = base_url + "/oauth/access_token"
+  authorize_url = "http://www.fitbit.com/oauth/authorize"
+  api_version = "/1"
+
+  session = OAuth1Session(fitbit_consumer_key(),
+                          fitbit_consumer_secret(),
+			  fitbit_oauth_token(),
+			  fitbit_oauth_secret())
+
+  today = date.today().isoformat()
+  url = base_url + api_version + "/user/-/activities/date/"+today+".json"
+  r = session.get(url, params={}, header_auth=True)
+  return int(r.json()['summary']['floors'])
+
+
+def get_floors_ytd():
   base_url = "https://api.fitbit.com"
   request_token_url = base_url + "/oauth/request_token"
   access_token_url = base_url + "/oauth/access_token"
@@ -30,6 +48,15 @@ def getYTDFloors():
   print "total: ", total
   return total
 
+def get_ytd_pace():
+  jan1 = date(date.today().year, 1, 1)
+  today = date.today()
+  delta = today - jan1
+
+  ytd_pace = (delta.days + 1) * 55
+
+  return ytd_pace
+
 def postTweet(messageBody):
   base_url = "https://api.twitter.com"
   api_version = "/1.1"
@@ -45,7 +72,35 @@ def postTweet(messageBody):
   r = session.post(url, data=params, header_auth=True)
   print r.json()
 
-#acquireFitbitTokens()
-num = getYTDFloors()
-tweet_text = 'Floors climbed year-to-date: ' + str(num)# + ' #fitbit #fitstats'
-postTweet(tweet_text)
+def send_direct_message(user, message_body):
+  base_url = "https://api.twitter.com"
+  api_version = "/1.1"
+
+  session = OAuth1Session(twitter_consumer_key(),
+                          twitter_consumer_secret(),
+			  twitter_oauth_token(),
+			  twitter_oauth_secret())
+			
+  url = base_url + api_version + "/direct_messages/new.json"
+  params = {'screen_name' : user, 'text' : message_body}
+
+  r = session.post(url, data=params, header_auth=True)
+  print r.json()
+
+
+def main():
+  #num = get_floors_ytd()
+  #tweet_text = 'Floors climbed year-to-date: ' + str(num) #+ ' #fitbit #fitstats'
+  #postTweet(tweet_text)
+
+  print get_ytd_pace()
+  print get_floors_today()
+
+  pace_delta = get_floors_ytd() - get_ytd_pace()
+  if pace_delta > 0:
+    send_direct_message('boxel', "You're ahead of pace by " + str(pace_delta))
+  else:
+    send_direct_message('boxel', "You're behind pace by " + str(abs(pace_delta)))
+
+if __name__ == "__main__":
+    main()
