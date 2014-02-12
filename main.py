6f582bb83471a6e6fe7f75d9d933d6ece2f71920
Matt Morris <matt@mattmorr.is>
2014-02-11 21:43:14 -0500

    Code cleanup

--- main.py
+++ main.py
@@ -7,19 +7,13 @@ from FitbitAuth import *
 from TwitterAuth import *
 
 def get_floors_ytd():
-  base_url = "https://api.fitbit.com"
-  request_token_url = base_url + "/oauth/request_token"
-  access_token_url = base_url + "/oauth/access_token"
-  authorize_url = "http://www.fitbit.com/oauth/authorize"
-  api_version = "/1"
-
   session = OAuth1Session(fitbit_consumer_key(),
                           fitbit_consumer_secret(),
 			  fitbit_oauth_token(),
 			  fitbit_oauth_secret())
 
   jan_first_date = str(date.today().year) + "-01-01"
-  url = base_url + api_version + "/user/-/activities/floors/date/"+jan_first_date+"/today.json"
+  url = "https://api.fitbit.com/1/user/-/activities/floors/date/"+jan_first_date+"/today.json"
   r = session.get(url, params={}, header_auth=True)
 
   # add values for each day
@@ -31,15 +25,12 @@ def get_floors_ytd():
   return total
 
 def postTweet(messageBody):
-  base_url = "https://api.twitter.com"
-  api_version = "/1.1"
-
   session = OAuth1Session(twitter_consumer_key(),
                           twitter_consumer_secret(),
 			  twitter_oauth_token(),
 			  twitter_oauth_secret())
 			
-  url = base_url + api_version + "/statuses/update.json"
+  url = "https://api.twitter.com/1.1/statuses/update.json"
   params = {'status' : messageBody}
 
   r = session.post(url, data=params, header_auth=True)
